17ba9486bfea1f35741b2817d75828d57bbb919c
eminbasbayan <emin.basbayan@icloud.com>
2026-01-22 22:45:28 +0300

    path module kullanımı

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const data = require("./data.json");
+const path = require("node:path");
 
-console.log("Emin");
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
+
+// console.log(path.extname(__filename));
+// console.log(path.extname("index.css"));
+
+// let parsedPath = path.parse(__filename);
+
+// console.log(parsedPath);
+
+// console.log(path.format(parsedPath));
+
+// console.log(path.isAbsolute("C:/Users/eminb/Desktop/2026-1-nodejs-bootcamp"));
+// console.log(path.isAbsolute("./data.json"));
+// console.log(__filename);
+
+// console.log(path.join("musteriler", "usernames", "faturalar", "fatura.pdf"));
+
+// console.log(path.resolve('folder1', 'folder2', 'index.html')); // Mutlak yol
+
+
+
+
+
+// console.log(__filename);
+// console.log(__dirname);
