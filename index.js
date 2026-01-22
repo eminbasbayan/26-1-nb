48d7793bc27d69257e6227b4b6ef9fcc45d8fe8f
eminbasbayan <emin.basbayan@icloud.com>
2026-01-22 21:49:18 +0300

    module wrapper - iife

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-// require("./add.js");
-
-const add = require("./add.js");
-const add2 = require("./add.js");
-
-const sum = add(3, 4);
-const sum2 = add2(5, 9);
-console.log(sum);
-console.log(sum2);
+require("./batman.js");
+require("./superman.js");
