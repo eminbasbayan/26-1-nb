8212a987e8b1c484369836f105605eb161b04cfd
eminbasbayan <emin.basbayan@icloud.com>
2026-01-24 21:45:29 +0300

    fs module

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const EventEmitter = require('node:events');
-const emitter = new EventEmitter();
+const fs = require("node:fs");
 
-emitter.on('orderPizza', (boyut, icindekiler) => {
-  console.log(`Bir ${boyut} pizza pişiriliyor. İçindekiler: ${icindekiler}`);
-});
+// Senkron dosya okuma
+// const txtFile = fs.readFileSync("./file.txt", "utf-8");
+// console.log(txtFile.toString());
+// console.log("sync");
 
-emitter.emit("orderPizza", "small", "peynir");
+// Asenkron dosya okuma
+/* fs.readFile("./file.txt", "utf-8", (err, data)=>{
+   if(err){
+      console.log(err);
+   }else{
+      console.log(data);
+   }
+})
+console.log("async"); */
+
+// Dosya içi yazıyı senkron değiştirme
+/* fs.writeFileSync("./file.txt", "Bu bir test dosyasıdır."); */
+
+// Dosya içi yazıyı asenkron değiştirme
+/* fs.writeFile("./file2.txt", "Emin Başbayan", (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Dosya başarıyla yazıldı!");
+
+    }
+}) */
+
+// Dosyaya sonuna yeni veri ekleme
+fs.writeFile("./file.txt", "Yeni Eklenen Veri", { flag: "a" }, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Dosya başarıyla yazıldı!");
+    }
+})
+
