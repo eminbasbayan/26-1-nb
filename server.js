e532a2623740a2e1e6fcb6e6bc418bc126049e7c
eminbasbayan <emin.basbayan@icloud.com>
2026-01-31 22:07:21 +0300

    http put update - body

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 // Middleware to parse JSON badies
 app.use(express.json());
 
-const users = [
+let users = [
   { id: 1, name: 'Ahmet', age: 25, email: 'ahmet@example.com' },
   { id: 2, name: 'Ayşe', age: 30, email: 'ayse@example.com' },
 ];
@@ -17,9 +17,29 @@ app.get('/', (req, res) => {
 
 app.post('/api/users', (req, res) => {
   console.log(req.body);
-  const newUsers = [...users, req.body]
+  const newUsers = [...users, req.body];
 
   res.json(newUsers);
+});
+
+app.put('/api/users', (req, res) => {
+  const { id: userId, email } = req.body;
+
+  const findUser = users.find((user) => user.id === userId);
+
+  if (findUser) {
+    users = users.map((user) => {
+      if (user.id === findUser.id) {
+        return { ...user, email };
+      }
+      return user;
+    });
+    res.json({ success: true, users });
+  } else {
+    res.json({ success: false, message: 'Kullanıcı Bulunamadı!' });
+  }
+
+  // findUser.email = email;
 });
 
 app.use((req, res) => {
