46b1bb0add2f348f6fe3d6b5f0562deca875f94b
eminbasbayan <emin.basbayan@icloud.com>
2026-02-05 22:07:57 +0300

    cors whitelist ayarları

--- server.js
+++ server.js
@@ -5,7 +5,28 @@ const fs = require('node:fs');
 const path = require('node:path');
 const PORT = 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    // İzin verilen origins listesi
+    const whiteList = [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://www.google.com',
+    ];
+
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS politikası tarafından engellendiniz!'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeader: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // Middleware to parse JSON badies
 app.use(express.json());
