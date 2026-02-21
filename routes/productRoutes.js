cd1df46b3e9786b8f3ffee0223235313394f8eb7
eminbasbayan <emin.basbayan@icloud.com>
2026-02-21 23:05:10 +0300

    category ve product express validator

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,12 +2,27 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const validate = require('../validators/validate');
-const { createProductValidator } = require('../validators/productValidators');
+const {
+  createProductValidator,
+  productIdParamValidator,
+  getProductsByCategoriesValidator,
+  updateProductValidator,
+} = require('../validators/productValidators');
 
 router.get('/',  productController.getAllProducts);
-router.get('/by-categories', productController.getProductsByCategories);
+router.get(
+  '/by-categories',
+  getProductsByCategoriesValidator,
+  validate,
+  productController.getProductsByCategories,
+);
 router.post('/', createProductValidator, validate, productController.createNewProduct);
-router.put('/:productId', productController.updateProduct);
-router.delete('/:productId', productController.deleteProduct);
+router.put('/:productId', updateProductValidator, validate, productController.updateProduct);
+router.delete(
+  '/:productId',
+  productIdParamValidator,
+  validate,
+  productController.deleteProduct,
+);
 
 module.exports = router;
