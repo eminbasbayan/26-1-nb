056fa109b33ad3f6f6ddf3081c579dcdc51d00ca
eminbasbayan <emin.basbayan@icloud.com>
2026-02-26 21:56:50 +0300

    user ve refresh token model oluşturuldu

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email zorunludur!'], // Validasyon
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, 'Parola zorunludur!'],
+      minlength: [6, 'Parola en az 6 karakter olmalıdır!'],
+    },
+  },
+  {
+    timestamps: true, // createdAt ve updatedAt otomatik eklenir
+  },
+);
+
+module.exports = mongoose.model('User', userSchema); // categories
