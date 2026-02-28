099c7682f1502132680d6b507903accd338e5cd8
eminbasbayan <emin.basbayan@icloud.com>
2026-02-28 22:08:26 +0300

    product routes da swagger kullanımı

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,12 +11,55 @@ const {
 const { verifyAccessToken } = require('../middleware/auth');
 const { authorizeRoles } = require('../middleware/roles');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *         name:
+ *           type: string
+ *         price:
+ *           type: number
+ *         category:
+ *           type: object
+ *         description:
+ *           type: string
+ *         inStock:
+ *           type: boolean
+ *         tags:
+ *           type: array
+ *           items:
+ *             type: string
+ */
+
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Tum urunleri listeler
+ *     tags:
+ *       - Products
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Basarili urun listesi
+ *       401:
+ *         description: Yetkilendirme hatasi (token yok/gecersiz)
+ *       403:
+ *         description: Rol yetkisi yetersiz
+ */
 router.get(
   '/',
   verifyAccessToken,
   authorizeRoles('admin'),
   productController.getAllProducts,
 );
+
 router.get(
   '/by-categories',
   getProductsByCategoriesValidator,
@@ -41,6 +84,7 @@ router.put(
   validate,
   productController.updateProduct,
 );
+
 router.delete(
   '/:productId',
   verifyAccessToken,
